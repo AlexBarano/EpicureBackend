bd134d3230c38ce2372353693688ee16488772e0
Alex Baranov <alexb@moveo.co.il>
2022-04-11 16:55:39 +0300

    Adding connect to db

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from "express";
+const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
+import connect from "./db/connect.js";
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.listen(process.env.port || 3000);
-console.log("Web Server is listening at port " + (process.env.port || 3000));
+const port = process.env.PORT || 3000;
+
+const startServer = async () => {
+  await connect(process.env.MONGODB_URL);
+  app.listen(process.env.port || 3000);
+
+  console.log("Web Server is listening at port " + port);
+};
+
+startServer();
